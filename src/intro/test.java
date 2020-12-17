846c17c34f054127912a60ea3d0e577275bd44a2
Alonso <aastorgaestevan4@gmail.com>
2020-12-16 21:47:30 -0400

    se instancia persona en test

--- src/intro/test.java
+++ src/intro/test.java
@@ -1,9 +1,11 @@
 package intro;
 
+import java.io.IOException;
+
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Persona p = new Persona();
 
 	}
 
