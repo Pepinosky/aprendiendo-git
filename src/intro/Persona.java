0eed58cb74e7b361200b789ff958d70542feab97
Alonso <aastorgaestevan4@gmail.com>
2020-12-16 21:44:53 -0400

    ingreso de nombre y apellido

--- src/intro/Persona.java
+++ src/intro/Persona.java
@@ -1,5 +1,21 @@
 package intro;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Persona {
+	private String nombre, apellido;
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader entrada = new BufferedReader(isr);
+	
+	public Persona() throws IOException {		
+		String nombre= entrada.readLine();
+		String apellido= entrada.readLine();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+
 
 }
